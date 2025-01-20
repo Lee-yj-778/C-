18a5a1190f97d12dedead6472bf0b0dfd83f19ca
Lee-yj-778 <youmga778@naver.com>
2025-01-20 14:55:33 +0900

    Add files via upload

---
+++ 소수찾기.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main() {
+	int index;
+	int primes = 0;
+
+	for (int i = 2; i < 1000; i++) {
+		for (index = 2; index < i; index++) {
+			if (i % index == 0) {
+				break;
+			}
+		}
+		if (index == i) {
+			primes++;
+			printf("%4d%s", i, (primes % 5) == 0 ? "\n" :" ");
+		}
+	}
+	printf("%d", primes);
+		return 0;
+	}
